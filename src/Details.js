bbd325a52fe85347b6352e7b7405c79e266fbf37
Uday Kota <udaykota@Udays-MacBook-Air.local>
2024-10-13 22:24:57 +0530

    first commit

---
+++ src/Details.js
@@ -0,0 +1,145 @@
+// Enter all your detials in this file
+// Logo images
+// import logogradient from "./assets/logo.svg";
+// import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/uday.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+// import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+// import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import cplusplus from "./assets/techstack/c++.png"
+import materialui from "./assets/techstack/material_UI.png"
+import MongoDB from "./assets/techstack/mongodb.png"
+import jupyter from "./assets/techstack/jupyter.png"
+import mysql from "./assets/techstack/mysql.png"
+// import figma from "./assets/techstack/figma.png";
+
+
+// Porject Images
+import getmarried from "./assets/projects/getmarried.png";
+import chess from "./assets/projects/chess.png";
+import realchat from "./assets/projects/chat.jpg";
+
+
+// Logos
+// export const logos = {
+//   logogradient: logogradient,
+//   logo: logo,
+// };
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Uday Kota",
+  tagline: "I build things for web",
+  img: profile,
+  about: `I'm a passionate full-stack developer with expertise in building scalable web applications using the MERN stack. I enjoy solving complex problems and delivering efficient, user-focused solutions. My strengths lie in backend development, database management, and creating seamless user experiences with clean code.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/uday-kota-1024b0226/",
+  github: "https://github.com/UdayKota03",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/_.uday.kota._?igsh=MTdhbXg5a3ZjNnIzcA%3D%3D&utm_source=qr",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Data Analyst Intern",
+    Company: `MarryFest Inc`,
+    Location: "Remote",
+    Type: "Internship",
+    Duration: "Jan 2023 - May 2024",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Computer Science & Engineering",
+    Company: `Indian Institute of Information Technology Sonepat`,
+    Location: "Haryana",
+    Type: "CGPA - 8.53",
+    Duration: "Dec 2021 - Present",
+  },
+  {
+    Position: "Board of Intermediate Education",
+    Company: `Bhashyam Junior College`,
+    Location: "Guntur",
+    Type: "Total Marks - 977/1000",
+    Duration: "Aug 2019 - May 2021",
+  },
+  {
+    Position: "Board of Secondary Education",
+    Company: `Bhashyam Public School`,
+    Location: "Rajahmundry",
+    Type: "CGPA - 10",
+    Duration: "May 2018 - May 2019",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  cplusplus: cplusplus,
+  materialui: materialui,
+  MongoDB: MongoDB,
+  jupyter: jupyter,
+  mysql: mysql
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "GetMarried",
+    image: getmarried,
+    description: `GetMarried is a full-stack matrimony platform built with Node.js, Express, and MongoDB. It enables user registration, profile creation, and matchmaking. With secure authentication, email verification, and real-time interest notifications, the platform improves efficiency by 30% and boosts user engagement by 25% through user-friendly dashboards.`,
+    techstack: "HTML/CSS, JavaScript, React.js, Node.js, Express.js, MongoDB, JWT, Zustand",
+    previewLink: "https://getmarried.onrender.com/",
+    githubLink: "https://github.com/UdayKota03/GetMarried",
+  },
+  {
+    title: "RealChat",
+    image: realchat,
+    description: `RealChat is a full-stack, real-time chat application built with the MERN stack, integrating Socket.io for instant messaging and user status updates. It features secure authentication and authorization using JWT for managing user access. The app is designed with TailwindCSS, Daisy UI, and Zustand for a seamless user experience.`,
+    techstack: "HTML/CSS, JavaScript, React.js, Node.js, Express.js, MongoDB, JWT, Zustand, Socket.io",
+    previewLink: "https://realchat-ey8v.onrender.com/",
+    githubLink: "https://github.com/UdayKota03/RealChat",
+  },
+  {
+    title: "Chess-WebApp",
+    image: chess,
+    description: `Chess-WebApp is a chess platform built with Node.js, Express.js, and MongoDB, offering real-time gameplay features. WebSocket integration enables instant updates and notifications, enhancing user engagement during live matches. The user-friendly interface, designed with TailwindCSS, ensures intuitive controls and a responsive experience across all devices, making gameplay seamless and accessible.`,
+    techstack: "Node.js, Express.js, TailwindCSS, MongoDB, Socket.io",
+    previewLink: "https://chess-webapp-klij.onrender.com/",
+    githubLink: "https://github.com/UdayKota03/Chess-WebApp",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "udaykota4u@gmail.com",
+  phone: "+91 8555038018",
+};
---
+++ src/Pages/About.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Work from "../Components/Work";
+import { personalDetails, workDetails, eduDetails } from "../Details";
+
+function About() {
+  return (
+    <main className="container mx-auto max-width pt-10 pb-20 ">
+      <section>
+        <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          About Me
+        </h1>
+        <p className="text-content py-8 lg:max-w-3xl">
+          {personalDetails.about}
+        </p>
+        <a
+          href="https://drive.google.com/file/d/1yFEEQxJbEb6x15wyePadxD3qSRboXuDO/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block px-6 py-3 mt-4 mb-4 text-white bg-slate-400 hover:bg-slate-500 rounded-md font-semibold"
+        >
+          Resume
+        </a>
+      </section>
+      <section>
+        <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Work Experience
+        </h1>
+        {React.Children.toArray(
+          workDetails.map(({ Position, Company, Location, Type, Duration }) => (
+            <Work
+              position={Position}
+              company={Company}
+              location={Location}
+              type={Type}
+              duration={Duration}
+            />
+          ))
+        )}
+      </section>
+      <section>
+        <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Education
+        </h1>
+        {React.Children.toArray(
+          eduDetails.map(({ Position, Company, Location, Type, Duration }) => (
+            <Work
+              position={Position}
+              company={Company}
+              location={Location}
+              type={Type}
+              duration={Duration}
+            />
+          ))
+        )}
+      </section>
+    </main>
+  );
+}
+
+export default About;
